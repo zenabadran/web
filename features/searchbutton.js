4cf747e9bfd50b28b123b78c27bfd3115ae42352
zena badran <zenabadran2@gmail.com>
2023-05-13 20:35:29 +0300

    edit

---
+++ features/searchbutton.js
@@ -0,0 +1,4 @@
+const searchcontainer = document.getElementById("search-container");
+document.getElementById("search-button").onclick = function myFunction() {
+  searchcontainer.style.display = "block";
+};
