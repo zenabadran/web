7a24282c5474fb266c812db039de00c79bd90272
zena badran <zenabadran2@gmail.com>
2023-05-13 22:37:54 +0300

    edit

--- features/searchbutton.js
+++ features/searchbutton.js
@@ -1,13 +1,11 @@
 const searchcontainer = document.getElementById("search-container");
 
-document.getElementById("search-button").addEventListener("click", function(event) {
-  searchcontainer.style.display = "block";
-  event.stopImmediatePropagation();
-});
+document
+  .getElementById("search-button")
+  .addEventListener("click", function (event) {
+    searchcontainer.style.display = "block";
+    event.stopImmediatePropagation();
+  });
 document.body.addEventListener("click", function (evt) {
-   
-    searchcontainer.style.display = "none";
-    
-}
-
-);
+  searchcontainer.style.display = "none";
+});
