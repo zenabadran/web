a12ea166dd7884b24d3ce37d449f22f20535dbfa
zena badran <zenabadran2@gmail.com>
2023-05-14 11:36:05 +0300

    edit

--- features/searchbutton.js
+++ features/searchbutton.js
@@ -1,21 +1,22 @@
 const searchcontainer = document.getElementById("search-container");
 const dropdownmaincontainer=document.getElementsByClassName('dropdown-main-container');
-
+const hoverdropdownlist =document.getElementsByClassName('hover-dropdown-list');
 document
   .getElementById("search-button")
   .addEventListener("click", function (event) {
     searchcontainer.style.display = "block";
     for(let i=0 ;i<6;i++){
-        dropdownmaincontainer[i].style.zIndex="-2" ;
-       
+        // dropdownmaincontainer[i].style.zIndex="-2" ;
+        hoverdropdownlist[i].style.pointerEvents="none";
+        hoverdropdownlist[i].style.cursor = "pointer";
     }
-    document.getElementById('hover-dropdown-list').style.pointerEvents="none"
+    
     event.stopImmediatePropagation();
   });
 document.body.addEventListener("click", function (evt) {
   searchcontainer.style.display = "none";
-  document.getElementById('hover-dropdown-list').style.pointerEvents="auto"
   for(let i=0 ;i<6;i++){
-    dropdownmaincontainer[i].style.zIndex="3" ;
+    hoverdropdownlist[i].style.pointerEvents="auto";
 }
+searchcontainer.reset();
 });
