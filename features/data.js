2f3c0b3730b8d426e8629e97dc59c4aa8d54ecba
zena badran <zenabadran2@gmail.com>
2023-05-15 23:06:04 +0300

    edit

--- features/data.js
+++ features/data.js
@@ -6,15 +6,27 @@
 // };
 // fetchProducts('https://fakestoreapi.com/products?limit=5');
 // });
-const featuredcategoriestitles =document.getElementsByClassName('featured-categories-titles');
-const featuredcategoriesimg= document.getElementsByClassName("featured-categories-img");
-fetch('https://fakestoreapi.com/products?limit=7')
-            .then(res=>res.json())
-            .then(json=>{
-                for(let i =0 ;i<7;i++){
-                    featuredcategoriestitles[i].innerHTML=json[i].title;
-                    featuredcategoriesimg[i].src=json[i].image;
-        }
-        
-
-            })
+const featuredcategoriestitles = document.getElementsByClassName(
+  "featured-categories-titles"
+);
+const featuredcategoriesimg = document.getElementsByClassName(
+  "featured-categories-img"
+);
+const recentimage = document.getElementsByClassName("recent-image");
+const recentnewstitle = document.getElementsByClassName("recent-news-title");
+const recentnewsdiscreption = document.getElementsByClassName(
+  "recent-news-discreption"
+);
+fetch("https://fakestoreapi.com/products?limit=9")
+  .then((res) => res.json())
+  .then((json) => {
+    for (let i = 0; i < 7; i++) {
+      featuredcategoriestitles[i].innerHTML = json[i].title;
+      featuredcategoriesimg[i].src = json[i].image;
+    }
+    for (let i = 0; i < 9; i++) {
+      recentnewstitle[i].innerHTML = json[i].title;
+      recentnewsdiscreption[i].innerHTML = json[i].description;
+      recentimage[i].src = json[i].image;
+    }
+  });
--- features/recentnewsslider.js
+++ features/recentnewsslider.js
@@ -1,26 +1,22 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    // el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.0": {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      // "@0.00": {
-      //   slidesPerView: 1,
-      //   spaceBetween: 10,
-      // },
-      "@01.5": {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      "@1.8": {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      "@3.50": {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
+    "@1.7": {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-  });
+    "@2.2": {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
