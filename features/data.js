ab063a55dabb2482fb531c461e062c481ce84783
zena badran <zenabadran2@gmail.com>
2023-05-18 17:53:53 +0300

    edit

--- features/data.js
+++ features/data.js
@@ -6,25 +6,17 @@
 // };
 // fetchProducts('https://fakestoreapi.com/products?limit=5');
 // });
-const featuredcategoriestitles = document.getElementsByClassName(
-  "featured-categories-titles"
+
+let product = document.getElementById("swiper-wrapper");
+let featuredcategorieswrapper = document.getElementById(
+  "featured-categories-wrapper"
 );
-const featuredcategoriesimg = document.getElementsByClassName(
-  "featured-categories-img"
-);
-const recentimage = document.getElementsByClassName("recent-image");
-const recentnewstitle = document.getElementsByClassName("recent-news-title");
-const recentnewsdiscreption = document.getElementsByClassName("recent-news-discreption");
-const category = document.getElementsByClassName("category");
-const recentlyviewedproductsimg = document.getElementsByClassName("recently-viewed-products-img");
-const nodeList = document.querySelectorAll(".card-description h5");
-const rate = document.getElementsByClassName("rate");
-let product= document.getElementById("swiper-wrapper");
+let recentlyviewedproducts = document.getElementById("swiper-wrapper2");
 fetch("https://fakestoreapi.com/products?limit=12")
   .then((res) => res.json())
   .then((json) => {
-    for (let i = 0; i < json.length; i++){
-      product.innerHTML +=`
+    for (let i = 0; i < json.length; i++) {
+      product.innerHTML += `
       <div  class="swiper-slide">
       <div class="recent-news-img">
        <img class="recent-image"  src="${json[i].image}" alt="clothes">
@@ -34,25 +26,27 @@ fetch("https://fakestoreapi.com/products?limit=12")
       <a href="javascript:;">Read More<span class="material-symbols-outlined">arrow_right_alt</span></a>
       </div>
       `;
+      featuredcategorieswrapper.innerHTML += `
+      <div class="featured-categories-card">
+        <img src="${json[i].image}" alt="boiler" />
+        <a href="javascript:;">${json[i].title}<span class="material-symbols-outlined">arrow_right_alt</span></a>
+      </div>
+      `;
+      recentlyviewedproducts.innerHTML += `
+      <div class="swiper-slide Slider2">
+          <div class="slide-wrapper">
+              <div class="gradient">
+                  <span> ${json[i].category}</span>
+              </div>
+              <img src="${json[i].image}" alt="boiler" />
 
+              <div class="card-description">
+                 <h5>${json[i].title}</h5>
+                 <span> <span> CB Part #:</span> <span>${json[i].rating.rate}</span> </span>
+                 <button>VIEW DETAILS</button>
+              </div>
+            </div>
+        </div>
+      `;
     }
-    // for (let i = 0; i < 7; i++) {
-    //   featuredcategoriestitles[i].innerHTML = json[i].title;
-    //   featuredcategoriesimg[i].src = json[i].image;
-    // }
-    // for (let i = 0; i < 9; i++) {
-    //   recentnewstitle[i].innerHTML = json[i].title;
-    //   recentnewsdiscreption[i].innerHTML = json[i].description;
-    //   recentimage[i].src = json[i].image;
-    // }
-    // for (let i = 0; i < 5; i++) {
-    //   category[i].innerHTML = json[i].category;
-    //   recentlyviewedproductsimg[i].src= json[i].image;
-    //   nodeList[i].innerHTML = json[i].title;
-    //   rate[i].innerHTML =json[i].rating.rate;
-    // }
-   
-
   });
-  
-  
