1575da9c0eff4e18e0c3c751a4e9d1146e94401e
zena badran <zenabadran2@gmail.com>
2023-05-18 17:14:40 +0300

    edit

--- features/data.js
+++ features/data.js
@@ -19,24 +19,40 @@ const category = document.getElementsByClassName("category");
 const recentlyviewedproductsimg = document.getElementsByClassName("recently-viewed-products-img");
 const nodeList = document.querySelectorAll(".card-description h5");
 const rate = document.getElementsByClassName("rate");
-fetch("https://fakestoreapi.com/products?limit=9")
+let product= document.getElementById("swiper-wrapper");
+fetch("https://fakestoreapi.com/products?limit=12")
   .then((res) => res.json())
   .then((json) => {
-    for (let i = 0; i < 7; i++) {
-      featuredcategoriestitles[i].innerHTML = json[i].title;
-      featuredcategoriesimg[i].src = json[i].image;
+    for (let i = 0; i < json.length; i++){
+      product.innerHTML +=`
+      <div  class="swiper-slide">
+      <div class="recent-news-img">
+       <img class="recent-image"  src="${json[i].image}" alt="clothes">
+      </div>
+      <h2 class="recent-news-title">${json[i].title}</h2>
+      <p class="recent-news-discreption">${json[i].description}</p>
+      <a href="javascript:;">Read More<span class="material-symbols-outlined">arrow_right_alt</span></a>
+      </div>
+      `;
+
     }
-    for (let i = 0; i < 9; i++) {
-      recentnewstitle[i].innerHTML = json[i].title;
-      recentnewsdiscreption[i].innerHTML = json[i].description;
-      recentimage[i].src = json[i].image;
-    }
-    for (let i = 0; i < 5; i++) {
-      category[i].innerHTML = json[i].category;
-      recentlyviewedproductsimg[i].src= json[i].image;
-      nodeList[i].innerHTML = json[i].title;
-      rate[i].innerHTML =json[i].rating.rate;
-    }
+    // for (let i = 0; i < 7; i++) {
+    //   featuredcategoriestitles[i].innerHTML = json[i].title;
+    //   featuredcategoriesimg[i].src = json[i].image;
+    // }
+    // for (let i = 0; i < 9; i++) {
+    //   recentnewstitle[i].innerHTML = json[i].title;
+    //   recentnewsdiscreption[i].innerHTML = json[i].description;
+    //   recentimage[i].src = json[i].image;
+    // }
+    // for (let i = 0; i < 5; i++) {
+    //   category[i].innerHTML = json[i].category;
+    //   recentlyviewedproductsimg[i].src= json[i].image;
+    //   nodeList[i].innerHTML = json[i].title;
+    //   rate[i].innerHTML =json[i].rating.rate;
+    // }
    
 
   });
+  
+  
--- features/view-detail-button.js
+++ features/view-detail-button.js
@@ -1,7 +1,23 @@
-const viewbutton = document.querySelectorAll(".card-description button");
-for(let i=0 ;i< viewbutton.length; i++){
-    viewbutton[i].addEventListener("click", function () {
-        window.location.href = "pages/recently-product-item-page.html";
+// const viewbutton = document.querySelectorAll(".card-description button");
+// const producttitle=document.getElementById("title");
 
-      });
-}
+// fetch("https://fakestoreapi.com/products?limit=9")
+//   .then((res) => res.json())
+//   .then((json) => {
+     
+//       // producttitle.innerHTML=json[1].title;
+    
+   
+// for(let i=0 ;i< viewbutton.length; i++){
+//   viewbutton[i].addEventListener("click", function () {
+//       window.location.href = "pages/recently-product-item-page.html";
+//      // x=json[i].title;
+     
+      
+//     });
+// }
+// // console.log(json[i].title);
+// // console.log(x);
+// // producttitle.innerHTML=x;
+
+// })
