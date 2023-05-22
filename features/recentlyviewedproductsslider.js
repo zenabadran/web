79a5fcf2201efcce1ce72252589388509d7936b6
zena badran <zenabadran2@gmail.com>
2023-05-22 13:16:24 +0300

    edit

--- features/recentlyviewedproductsslider.js
+++ features/recentlyviewedproductsslider.js
@@ -1,14 +1,11 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
+var swiper = new Swiper("#mySwiper", {
     spaceBetween: 10,
+    observer: true,
     pagination: {
     //   el: ".swiper-pagination",
       clickable: true,
     },
     breakpoints: {
-      0:{
-        slidesPerView: 1,
-      },
       765: {
         slidesPerView: 2,
         spaceBetween: 20,
--- features/recentnewsslider.js
+++ features/recentnewsslider.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".mySwiper2", {
   slidesPerView: 1,
   spaceBetween: 10,
+  observer: true,
   pagination: {
     // el: ".swiper-pagination",
     clickable: true,
