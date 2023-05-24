978052471b12a36918fce9a3f4f0ca1c581c067f
zena badran <zenabadran2@gmail.com>
2023-05-24 12:02:50 +0300

    edit

--- scripts/data.js
+++ scripts/data.js
@@ -10,12 +10,12 @@ fetch("https://fakestoreapi.com/products?limit=12")
     for (let i = 0; i < json.length; i++) {
       product.innerHTML += `
       <div  class="swiper-slide">
-      <div class="recent-news-img">
-       <img class="recent-image"  src="${json[i].image}" alt="clothes">
-      </div>
-      <h2 class="recent-news-title">${json[i].title}</h2>
-      <p class="recent-news-discreption">${json[i].description}</p>
-      <a href="javascript:;">Read More<span class="material-symbols-outlined">arrow_right_alt</span></a>
+        <div class="recent-news-img">
+          <img class="recent-image"  src="${json[i].image}" alt="clothes">
+        </div>
+        <h2 class="recent-news-title">${json[i].title}</h2>
+        <p class="recent-news-discreption">${json[i].description}</p>
+        <a href="javascript:;">Read More<span class="material-symbols-outlined">arrow_right_alt</span></a>
       </div>
       `;
       featuredcategorieswrapper.innerHTML += `
@@ -44,7 +44,7 @@ fetch("https://fakestoreapi.com/products?limit=12")
     const viewbutton = document.querySelectorAll(".card-description button");
     for(let i=0 ;i< viewbutton.length; i++){
       viewbutton[i].addEventListener("click", function () {
-           window.location.href = "pages/recently-product-item-page.html?page/"+json[i].id;
+           window.location.href = "views/recently-product-item-page.html?page/"+json[i].id;
         });
     }
   });
